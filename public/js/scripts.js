a52d5b3844632c3859171fb113568daae25cd272
london804 <alexechaparro@gmail.com>
2018-02-27 23:00:39 -0500

    responsive nav logic works

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,6 +10,8 @@ var ul = "<ul>" + "<ul>";
 var url = '';
 var navItems = document.querySelector('.nav-items');
 var background = document.querySelector('.background');
+var count;
+var callout = document.querySelectorAll('.background, .nav-close, .nav-item')
 
 
 xhr.onload = function() {
@@ -45,41 +47,65 @@ xhr.send();
 //nav
     if(navItems) {
         navItems.addEventListener("click", function(e) {
-            var callout = document.querySelectorAll('.background, .nav-close, .nav-item')
             console.log(e.target.closest('.nav-item'));
 
             if (background.style.display !== "block") {
                 background.style.display = "block";
 
-                for( let i = 0; i < e.target.closest('.nav-items').children.length; i++ ) {
-                    e.target.closest('.nav-items').children[i].classList.remove('active');
-                }
+                select(e);
 
-                e.target.closest('.nav-item').classList.add('active');
+            } else if (background.style.display === "block") {
+                    
+                select(e);
 
-                if (background.style.display === "block") {
-                    callout.forEach(function(elem) {
-                        elem.addEventListener("click", function(event) {
-                            background.style.display = "none";
-                            console.log('target', e.target);
-                            e.target.closest('.nav-item').classList.remove('active');
-                        }); 
-                    });
-                }
+            }  
 
-            } else {
-                background.style.display = "none";
-                e.target.closest('.nav-item').classList.remove('active');
+            // else {
 
-            }
+            //     if(count === false) {
+            //         count = true;
+            //         console.log('inner', count)
+            //         select(e);
+
+
+            //     } else {
+            //         background.style.display = "none";
+            //         e.target.closest('.nav-item').classList.remove('active');
+            //     }
+            // }
         })
     }
 
+    function select(e){
+
+        // go through all of the items on the nav and remove the active class
+        for( let i = 0; i < e.target.closest('.nav-items').children.length; i++ ) { 
+            e.target.closest('.nav-items').children[i].classList.remove('active');
+        }
+        // add the active class to the nav you just clicked
+        e.target.closest('.nav-item').classList.add('active');
+
+        // add an event listener to all places that can close the nav
+        if (background.style.display === "block") {
+            callout.forEach(function(elem) {
+                elem.addEventListener("click", function(event) {
+                    background.style.display = "none";
+                    console.log('target', e.target);
+                    e.target.closest('.nav-item').classList.remove('active');
+                }); 
+            });
+        }
+    }
+
+    var navOpen = document.querySelector('.nav-open');
+    var navHeader = document.querySelector('.nav-header');
+    var navClose = document.querySelector('.nav-close');
+
     function slide(e) {
-        
-        var navOpen = document.querySelector('.nav-open');
-        var navHeader = document.querySelector('.nav-header');
-        var navClose = document.querySelector('.nav-close');
+
+        count = false;
+
+        console.log('count', count);
 
         if (navItems.classList.length === 1) {
             navItems.classList.add("animate"); // nav ul
@@ -87,10 +113,15 @@ xhr.send();
             navHeader.classList.add("animate"); // elephant logo
             console.log(e.closest('.nav'));
             e.closest('.nav').classList.add('active'); // nav
+
             setTimeout(close, 500);
             function close() {
                 navClose.classList.remove("animate");
             }
+
+            background.style.display = "block";
+
+            check(e)
     
         } else {
             navItems.classList.remove("animate"); // nav ul
@@ -111,10 +142,40 @@ xhr.send();
                 // e.style.display = 'block'
             } else {
                 e.classList.add("animate");
-                // e.style.display = "none";
-                
+                // e.style.display = "none"; 
             }
+            background.style.display = "none";
         }
+    }
+
+    function check() {
+        background.addEventListener("click", function(){
+            console.log('check');
+            close()
+            
+        })
+    }
+
+    function close() {
+         navItems.classList.remove("animate"); // nav ul
+         navHeader.classList.remove("animate"); //
+         document.querySelector('.nav').classList.remove('active');
+         navClose.classList.remove('animate');
+         background.style.display = "none";
+
+         setTimeout(open, 500);
+        function open() {
+            navOpen.classList.remove("animate");
+        }
+
+        // if(e.classList.length === 2) { // animate icons
+        //     e.classList.remove('animate'); 
+        //     // e.style.display = 'block'
+        // } else {
+        //     e.classList.add("animate");
+        //     // e.style.display = "none"; 
+        // }
+
     }
 
     // function slideback(e) {
