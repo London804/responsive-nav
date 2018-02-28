093b19b6907a47516b0a93e558c9bd47daee0b44
london804 <alexechaparro@gmail.com>
2018-02-27 22:05:46 -0500

    cleaned up code and modified animation

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,19 +7,14 @@ var myObj = '';
 var nav = '';
 var x = '';
 var ul = "<ul>" + "<ul>";
-var wrapper = '';
 var url = '';
 var navItems = document.querySelector('.nav-items');
+var background = document.querySelector('.background');
 
 
 xhr.onload = function() {
     if (xhr.status === 200 && xhr.readyState === 4) {
-        //console.log(xhr.responseText);
         myObj = JSON.parse(this.responseText);
-        // for (x in myObj) {
-        //     nav += '<li>' + myObj.items[x] +'</li>';
-        //     console.log(myObj.items[x]);
-        // } // ask why this didn't work
         for (var i = 0; i < myObj.items.length; i++) {
                 
             var innerNav = '';
@@ -48,11 +43,8 @@ xhr.onload = function() {
 xhr.send();
 
 //nav
-    
     if(navItems) {
         navItems.addEventListener("click", function(e) {
-            var background = document.querySelector('.background');
-            // var body = document.querySelector('body');
             var callout = document.querySelectorAll('.background, .nav-close, .nav-item')
             console.log(e.target.closest('.nav-item'));
 
@@ -102,12 +94,17 @@ xhr.send();
     
         } else {
             navItems.classList.remove("animate"); // nav ul
-            navOpen.classList.remove("animate");  // nav open button
+            // navOpen.classList.remove("animate");  // nav open button
             navHeader.classList.remove("animate"); // elephant logo
             e.closest('.nav').classList.remove('active'); // nav
             // e.style.display = "block";                   // display of button clicked
             console.log('this', e.classList.length);
             e.classList.remove("animate");
+
+            setTimeout(open, 500);
+            function open() {
+                navOpen.classList.remove("animate");
+            }
 
             if(e.classList.length === 2) {
                 e.classList.remove('animate'); 
@@ -117,8 +114,6 @@ xhr.send();
                 // e.style.display = "none";
                 
             }
-
-
         }
     }
 
@@ -129,33 +124,3 @@ xhr.send();
     //  e.classList.add("animate");
 
     // }
-
-
-
-// var data = '';
-// console.log(this);
-
-// const url = '../data/navigation.json'
-// fetch(url) // Call the fetch function passing the url of the API as a parameter
-// .then(function() {
-//     // Your code for handling the data you get from the API
-// })
-// .catch(function() {
-//     // This is where you run code if the server returns any errors
-// });
-
- // function loadJSON(callback) {   
-
- //    var xobj = new XMLHttpRequest();
- //        xobj.overrideMimeType("application/json");
- //    xobj.open('GET', './api/navigation', true); 
- //    xobj.onreadystatechange = function () {
- //          if (xobj.readyState == 4 && xobj.status == "200") {
- //            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
- //            callback(xobj.responseText);
- //          }
- //    };
- //    xobj.send(null);  
- // }
-
- // loadJSON();
